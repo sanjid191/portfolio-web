f6d973643dd7b4967dffeff6ae4a6d5211d6e1f4
Sanjid Ahmmed <sanjid.sa191@gmail.com>
2023-08-24 16:41:29 +0600

    Loading Screen animation
    adding Loading Screen animation

---
+++ assets/js/loading.js
@@ -0,0 +1,9 @@
+// Wait for the entire page to load
+window.addEventListener('load', function() {
+    // Get the loader element
+    var loader = document.querySelector('.loader');
+  
+    // Hide the loader when the page is loaded
+    loader.style.display = 'none';
+  });
+  
